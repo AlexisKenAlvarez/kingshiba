1594e7984a277e3b15b1dcefa7edc209d1e03985
KoichiDevs <alexisken1432@gmail.com>
2023-02-27 23:26:44 +0800

    added about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,12 +16,22 @@ const About = () => {
                     </div>
 
                     <div className="flex gap-x-4 mt-20 w-full md:flex-row flex-col gap-y-4">
-                        <div className="relative w-full h-[20rem]">
-                            <div className="w-full h-full bg-white opacity-50"></div>
+                        <div className="relative w-full h-auto min-h-[22rem]">
+                            <div className="w-full h-full bg-white opacity-50 absolute z-0"></div>
+                            <div className="w-full h-full p-5 px-8 z-10 relative">
+                                <h1 className=' text-white text-center font-king text-3xl'>LOREM IPUSM</h1>
+
+                                <p className='text-center text-white mt-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
                         </div>
 
-                        <div className="relative w-full h-[20rem]">
-                            <div className="w-full h-full bg-white opacity-50"></div>
+                        <div className="relative w-full h-auto min-h-[22rem]">
+                            <div className="w-full h-full bg-white opacity-50 absolute z-0"></div>
+                            <div className="w-full h-full p-5 px-8 z-10 relative">
+                                <h1 className=' text-white text-center font-king text-3xl'>LOREM IPUSM</h1>
+
+                                <p className='text-center text-white mt-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
